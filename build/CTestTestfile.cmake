cdf7913c734d9917a1d46b75cbcfa293bd26d6fd
tanbro <tanbro@163.com>
2018-04-20 17:28:24 +0800

    remove: many file should not be in git

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/1B9074BA60C16502/works/huamei/unimrcp-plugin/xfyun_asr
-# Build directory: /mnt/1B9074BA60C16502/works/huamei/unimrcp-plugin/xfyun_asr/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
